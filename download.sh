55aab2ecdf753e20b379bb8ebc8581e26d3d60d0
Stephan Fuhrmann <stfu_os@freenet.de>
2021-07-10 13:27:31 +0200

    Don't repeat versions

--- download.sh
+++ download.sh
@@ -4,8 +4,10 @@ set -e
 SCRIPTHOME="$(cd $(dirname "$0"); pwd)"
 mkdir -p $SCRIPTHOME/bin
 
-wget https://repo1.maven.org/maven2/org/openapitools/openapi-generator-cli/5.1.1/openapi-generator-cli-5.1.1.jar
-mv openapi-generator-cli-*.jar $SCRIPTHOME/bin/openapi-generator-cli.jar
+VERSION=5.1.1
+wget https://repo1.maven.org/maven2/org/openapitools/openapi-generator-cli/${VERSION}/openapi-generator-cli-${VERSION}.jar
+mv openapi-generator-cli-${VERSION}.jar $SCRIPTHOME/bin/openapi-generator-cli.jar
 
-wget https://github.com/google/google-java-format/releases/download/v1.10.0/google-java-format-1.10.0-all-deps.jar
-mv google-java-format-1.10.0-all-deps.jar $SCRIPTHOME/bin/google-java-format.jar
+VERSION=1.10.0
+wget https://github.com/google/google-java-format/releases/download/v${VERSION}/google-java-format-${VERSION}-all-deps.jar
+mv google-java-format-${VERSION}-all-deps.jar $SCRIPTHOME/bin/google-java-format.jar

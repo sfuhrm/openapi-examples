8335cd74e58032ac83f885317ebced5803d0b5fc
Stephan Fuhrmann <stfu_os@freenet.de>
2021-07-11 15:07:37 +0200

    Update generator version

--- download.sh
+++ download.sh
@@ -4,7 +4,7 @@ set -e
 SCRIPTHOME="$(cd $(dirname "$0"); pwd)"
 mkdir -p $SCRIPTHOME/bin
 
-VERSION=5.1.1
+VERSION=5.2.0
 wget https://repo1.maven.org/maven2/org/openapitools/openapi-generator-cli/${VERSION}/openapi-generator-cli-${VERSION}.jar
 mv openapi-generator-cli-${VERSION}.jar $SCRIPTHOME/bin/openapi-generator-cli.jar
 

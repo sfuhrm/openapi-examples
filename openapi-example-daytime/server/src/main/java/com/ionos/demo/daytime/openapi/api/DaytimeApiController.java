cc1ebf5593e07a48660c19f9faa361f415cf772d
Stephan Fuhrmann <stephan.fuhrmann@ionos.com>
2021-07-13 18:32:14 +0200

    Implement the missing method

--- openapi-example-daytime/server/src/main/java/com/ionos/demo/daytime/openapi/api/DaytimeApiController.java
+++ openapi-example-daytime/server/src/main/java/com/ionos/demo/daytime/openapi/api/DaytimeApiController.java
@@ -1,6 +1,12 @@
 package com.ionos.demo.daytime.openapi.api;
 
+import java.time.OffsetDateTime;
 import java.util.Optional;
+
+import com.ionos.demo.daytime.openapi.model.DateTimeObject;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.context.request.NativeWebRequest;
@@ -20,5 +26,12 @@ public class DaytimeApiController implements DaytimeApi {
   @Override
   public Optional<NativeWebRequest> getRequest() {
     return Optional.ofNullable(request);
+  }
+
+  @Override
+  public ResponseEntity<DateTimeObject> dateTime() {
+    DateTimeObject dateTimeObject  = new DateTimeObject();
+    dateTimeObject.setDate(OffsetDateTime.now());
+    return new ResponseEntity<>(dateTimeObject, HttpStatus.OK);
   }
 }
